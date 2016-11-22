f018d273a16c09397eef34da216ac5daf622e896
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-11-22 21:02:58 +0100

    [FEATURE] Testing is fun

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 fm-error-analyzer
 =================
+[![Build Status](https://travis-ci.org/fairmanager/fm-error-analyzer.svg?branch=master)](https://travis-ci.org/fairmanager/fm-error-analyzer)
 
 Provides an **Angular** service that breaks down a passed object into a defined error object.
---
+++ test/fm-error-analyer.spec.js
@@ -0,0 +1,55 @@
+"use strict";
+
+describe( "fmErrorAnalyzer", function() {
+	var fmErrorAnalyzer;
+
+	beforeEach( module( "fmErrorAnalyzer" ) );
+
+	beforeEach( inject( function( _fmErrorAnalyzer_ ) {
+		fmErrorAnalyzer = _fmErrorAnalyzer_;
+	} ) );
+
+	it( "exists", function() {
+		expect( fmErrorAnalyzer ).to.be.defined;
+	} );
+
+	it( "parses generic HTTP error", function() {
+		var error = {
+			data       : {
+				status  : 500,
+				message : "relation \"image\" does not exist"
+			},
+			status     : 500,
+			statusText : "Internal Server Error"
+		};
+
+		var result = fmErrorAnalyzer.analyze( error );
+		expect( result ).to.be.an( "array" ).with.length( 2 );
+		expect( result[ 0 ].message ).to.equal( "Internal Server Error (500)" );
+		expect( result[ 1 ].message ).to.equal( "relation \"image\" does not exist" );
+	} );
+
+	it("parses more complex error", function() {
+		var error = {
+			status     : 503,
+			statusText : "Service Unavailable",
+			data       : {
+				error : {
+					info    : "The communication with the backend service failed",
+					message : "message could not be delivered",
+					inner   : {
+						address : "127.0.0.1",
+						port    : "7333",
+						code    : "ECONNREFUSED"
+					}
+				}
+			}
+		};
+
+		var result = fmErrorAnalyzer.analyze( error );
+		expect( result ).to.be.an( "array" ).with.length( 3 );
+		expect( result[ 0 ].message ).to.equal( "Service Unavailable (503)" );
+		expect( result[ 1 ].message ).to.equal( "The communication with the backend service failed (message could not be delivered)" );
+		expect( result[ 2 ].message ).to.equal( "ECONNREFUSED 127.0.0.1:7333" );
+	})
+} );
